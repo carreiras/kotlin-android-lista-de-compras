8c22c89144dd7c997f62b6b19228cf1de25f768d
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-05 14:30:08 -0300

    Criando a classe de adaptador do recyclerView: ItemsAdapter.kt

---
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemsAdapter.kt
@@ -0,0 +1,12 @@
+package carreiras.com.github.kotlin_android_lista_de_compras
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
+
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+}
