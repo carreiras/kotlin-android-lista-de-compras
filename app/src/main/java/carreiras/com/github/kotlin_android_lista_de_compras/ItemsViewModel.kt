5798437beac5634a74634494dd9a18d1c999b846
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-05 18:04:38 -0300

    Criando um livedata na ViewModel

--- app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemsViewModel.kt
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemsViewModel.kt
@@ -1,5 +1,6 @@
 package carreiras.com.github.kotlin_android_lista_de_compras
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
@@ -12,17 +13,21 @@ import androidx.lifecycle.ViewModel
 class ItemsViewModel : ViewModel() {
 
     private var items = mutableListOf<ItemModel>()
+    val itemsLiveData = MutableLiveData<List<ItemModel>>()
 
     fun addItem(name: String) {
         val item = ItemModel(
+            id = 0,
             name = name,
             onRemove = ::removeItem
         )
 
         items.add(item)
+        itemsLiveData.value = items
     }
 
     private fun removeItem(item: ItemModel) {
         items.remove(item)
+        itemsLiveData.value = items
     }
 }
