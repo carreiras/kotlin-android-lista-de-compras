73b307e2e0075f786f5a6dcc8777bcf3a0f7c68e
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-05 14:31:56 -0300

    Implementando membros de RecyclerView.Adapter: onCreateViewHolder, getItemCount e onBindViewHolder

--- app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemsAdapter.kt
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemsAdapter.kt
@@ -9,4 +9,16 @@ class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
