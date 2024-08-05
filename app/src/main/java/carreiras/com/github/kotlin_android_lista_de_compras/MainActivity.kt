2869f9afec8c8c32b81f434a2cb5ab00c0050802
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-05 13:23:37 -0300

    Chamando o arquivo de layout activity_main a partir da MainActivity.kt

--- app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_main)
 
     }
 }
