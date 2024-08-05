ccd1ce97c58268aba4ea43d246c0b3a27015b743
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-05 13:45:20 -0300

    Incluindo Toolbar ao projeto

--- app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/MainActivity.kt
@@ -3,6 +3,8 @@ package carreiras.com.github.kotlin_android_lista_de_compras
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,11 +14,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import carreiras.com.github.kotlin_android_lista_de_compras.ui.theme.KotlinandroidlistadecomprasTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
 
     }
 }
