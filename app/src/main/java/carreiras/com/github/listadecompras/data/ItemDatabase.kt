a24d798010c455d591744650f0f22f1596c84129
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-09-01 17:26:30 -0300

    Java Doc  - ItemDatabase.kt

--- app/src/main/java/carreiras/com/github/listadecompras/data/ItemDatabase.kt
+++ app/src/main/java/carreiras/com/github/listadecompras/data/ItemDatabase.kt
@@ -4,8 +4,24 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import carreiras.com.github.listadecompras.model.ItemModel
 
+/**
+ * Uma classe abstrata que serve como o ponto principal de acesso ao banco de dados SQLite para o aplicativo.
+ * Esta classe é anotada com @Database, listando todas as entidades que devem ser criadas no banco de dados.
+ * A classe estende RoomDatabase, que é uma classe do Room Persistence Library que contém o banco de dados.
+ * Dentro da classe, você define um método abstrato para cada @Dao que está associado ao banco de dados.
+ *
+ * @author Ewerton Carreira
+ * @version 1.0
+ * @since 2023-03-01
+ */
 @Database(entities = [ItemModel::class], version = 1)
 abstract class ItemDatabase : RoomDatabase() {
 
+    /**
+     * Este método abstrato serve como um ponto de acesso para a classe DAO.
+     * Quando este método é chamado, o Room irá gerar uma implementação.
+     *
+     * @return uma instância da classe DAO (ItemDao).
+     */
     abstract fun itemDao(): ItemDao
 }
