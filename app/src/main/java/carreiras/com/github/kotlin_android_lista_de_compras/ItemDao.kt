1d1c1e6c362f930abefab213b1b37f0b75953763
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-09-01 16:36:39 -0300

    Criando a classe DAO (Data Access Object)

---
+++ app/src/main/java/carreiras/com/github/kotlin_android_lista_de_compras/ItemDao.kt
@@ -0,0 +1,20 @@
+package carreiras.com.github.kotlin_android_lista_de_compras
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * FROM ItemModel")
+    fun getAll(): LiveData<List<ItemModel>>
+
+    @Insert
+    fun insert(item: ItemModel)
+
+    @Delete
+    fun delete(item: ItemModel)
+}
